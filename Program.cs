c895e34ce7207380bbb00e992e671b953d02db8b
Konf1ic <kenbui2809@gmail.com>
2024-04-19 16:28:28 +0700

    FindChar

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class CharacterCountDemo
+{
+    static void Main(string[] args)
+    {
+        // String 
+        string str = "Tskjvdsvdsolvdspdvksopvd.";
+
+        // Char to count
+        char character = 'k'; 
+
+        int count = 0;
+
+        // Count char
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == character)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Số lần xuất hiện của ký tự '{0}' trong chuỗi là: {1}", character, count);
+    }
+}
